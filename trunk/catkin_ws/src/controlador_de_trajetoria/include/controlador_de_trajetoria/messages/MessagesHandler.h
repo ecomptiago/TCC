2d7b8034ac5a1b8f523e5a890832a751ee53636f
Tiago Rafael Volkmann Coelho <t.coelho@cinq.com.br>
2015-09-03 12:41:48 -0300

    Adicionando mensagens

--- trunk/catkin_ws/src/controlador_de_trajetoria/include/controlador_de_trajetoria/messages/MessagesHandler.h
+++ trunk/catkin_ws/src/controlador_de_trajetoria/include/controlador_de_trajetoria/messages/MessagesHandler.h
@@ -10,10 +10,35 @@
 
 #include "std_msgs/String.h"
 #include "controlador_de_trajetoria/BaseRosNode.h"
+#include "controlador_de_trajetoria/Move_robot.h"
+
+const char* moveRobotAssyncTopic = "Message_handler/move_robot_assync";
+const char* targetPositionTopic = "Message_handler/target_position";
+const char* nodeName = "Message_handler";
 
 class MessagesHandler :public BaseRosNode{
+	private:
+		//Attributes
+		/**TODO - Pass this attributed to base class
+		to be inherited by derivated class. The actual
+		problem is that it can not be instantied before
+		calling ros::init*/
+		ros::NodeHandle nodeHandler;
+
 	public:
-		int main(int argc, char **argv);
+		//Constructors
+		MessagesHandler(int argc,char **argv);
+
+		//Destructor
+		virtual ~MessagesHandler() {} ;
+
+		//Methods
+		virtual int runNode();
+		bool subscribeToTopics();
+		bool createPublishers();
+		void proccessPositionToMoveRobot(
+				const controlador_de_trajetoria::Move_robot::ConstPtr& moveRobotPosition);
+
 };
 
 #endif /* INCLUDE_CONTROLADOR_DE_TRAJETORIA_MESSAGES_MESSAGESHANDLER_H_ */
