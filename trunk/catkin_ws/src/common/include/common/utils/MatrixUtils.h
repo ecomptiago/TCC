434836737062949fc2dcbd1943191e0c06ff10c8
Tiago rafael Volkmann Coelho <ecomptiago@hotmail.com>
2015-09-29 11:39:24 -0300

    Implementando metodo de eleminação de Gauss com pivoteamento parcial

--- trunk/catkin_ws/src/common/include/common/utils/MatrixUtils.h
+++ trunk/catkin_ws/src/common/include/common/utils/MatrixUtils.h
@@ -8,7 +8,25 @@
 #ifndef SRC_UTILS_MATRIXUTILS_H_
 #define SRC_UTILS_MATRIXUTILS_H_
 
+#include "iostream"
+
 class MatrixUtils {
+
+	private:
+		//Methods
+		void pivotingColumn(int lineIndex, int numberOfEquations,
+			float *linearEquationMatrix);
+
+		/*Methods that need to be implemented here, because of compiling
+		 *issues. More information at
+		 *http://stackoverflow.com/questions/8752837/undefined-reference-to-template-class-constructor
+		 */
+		template<class T>
+		static T getMatrixElement(int lineIndex, int columnIndex,
+			T *linearEquationMatrix, int columnNumber) {
+				return linearEquationMatrix[columnNumber * lineIndex + columnIndex];
+		}
+
 	public:
 		//Constructor
 		MatrixUtils() {};
--- trunk/catkin_ws/src/common/src/utils/MatrixUtils.cpp
+++ trunk/catkin_ws/src/common/src/utils/MatrixUtils.cpp
@@ -9,5 +9,36 @@
 
 bool MatrixUtils::applyGaussElimeliminationWithPartialPivotingAlgorithm(
 	float *linearEquationMatrix, int numberOfEquations, float *response) {
+		int i;
+		for(i = 0; i < numberOfEquations; i++) {
+			int k;
+			for(k = 0; k < numberOfEquations + 1; k++) {
+				std::cout <<
+					getMatrixElement<float>(i, k, linearEquationMatrix, numberOfEquations + 1)
+					<< " ";
+			}
+			std::cout << "\n";
+		}
+		std::cout << "\n";
+		for(i = 0; i < numberOfEquations; i++) {
+			std::cout << response[i] << "\t";
+		}
+
+//		int lineIndex;
+//		for(lineIndex = 0; lineIndex < numberOfEquations -1; lineIndex++) {
+//			pivotingColumn(lineIndex, numberOfEquations);
+//		}
+
 		return true;
+
 }
+
+void MatrixUtils::pivotingColumn(int lineIndex, int numberOfEquations,
+	float *linearEquationMatrix) {
+		int count;
+		for(count = 0; count < numberOfEquations; count++) {
+			int pivotingIndex = lineIndex + count;
+//			if(linearEquationMatrix[pivotingIndex])
+		}
+}
+
