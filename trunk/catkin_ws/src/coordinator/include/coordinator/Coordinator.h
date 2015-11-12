6d2a168ad1fd917367a2e0d3ffc70facccf3fdf4
Tiago Rafael Volkmann Coelho <t.coelho@cinq.com.br>
2015-11-12 12:24:34 -0200

    Subscribing to laser topic

--- trunk/catkin_ws/src/coordinator/include/coordinator/Coordinator.h
+++ trunk/catkin_ws/src/coordinator/include/coordinator/Coordinator.h
@@ -8,14 +8,31 @@
 #ifndef SRC_COORDINATOR_H_
 #define SRC_COORDINATOR_H_
 
+#include "algorithm"
 #include "common/BaseRosNode.h"
+#include "sensor_msgs/LaserScan.h"
 
 const char* nodeName = "Coordinator";
+const char* laserTopic = "/vrep/laser";
 
 class Coordinator : public BaseRosNode{
-public:
-	Coordinator(int argc, char **argv);
-	virtual ~Coordinator();
+
+	private:
+		//Atttributes
+		ros::NodeHandle nodeHandler;
+
+	public:
+		//Constructor
+		Coordinator(int argc, char **argv);
+
+		//Destructor
+		virtual ~Coordinator() {};
+
+		//Methods
+		void receivedLaserValues(
+			const sensor_msgs::LaserScan::ConstPtr& laserReading);
+		int runNode();
+		bool subscribeToTopics();
 };
 
 #endif /* SRC_COORDINATOR_H_ */
--- trunk/catkin_ws/src/coordinator/src/Coordinator.cpp
+++ trunk/catkin_ws/src/coordinator/src/Coordinator.cpp
@@ -7,16 +7,41 @@
 
 #include "coordinator/Coordinator.h"
 
+//Constructor
 Coordinator::Coordinator(int argc, char **argv) :
-BaseRosNode(argc, argv, nodeName){
-	// TODO Auto-generated constructor stub
-
+	BaseRosNode(argc, argv, nodeName){
 }
 
-Coordinator::~Coordinator() {
-	// TODO Auto-generated destructor stub
+//Methods
+int Coordinator::runNode() {
+	ROS_INFO("Running node");
+	ros::spin();
+	BaseRosNode::shutdownAndExit(nodeName);
+}
+
+bool Coordinator::subscribeToTopics() {
+	ROS_INFO("Subscribing to topics");
+	return addSubscribedTopic<const sensor_msgs::LaserScan::ConstPtr&, Coordinator>(nodeHandler,laserTopic,
+		&Coordinator::receivedLaserValues,this);
+}
+
+//Callback
+void Coordinator::receivedLaserValues(
+	const sensor_msgs::LaserScan::ConstPtr& laserReading) {
+	ROS_INFO("%f",
+		std::max_element(laserReading->ranges.begin(), laserReading->ranges.end()));
 }
 
 //Main
 int main(int argc, char **argv) {
+	Coordinator coordinator(argc,argv);
+	try{
+		if(coordinator.subscribeToTopics()) {
+			return coordinator.runNode();
+		} else {
+			BaseRosNode::shutdownAndExit(nodeName);
+		}
+	} catch (std::exception &e) {
+		BaseRosNode::shutdownAndExit(nodeName);
+	}
 }
