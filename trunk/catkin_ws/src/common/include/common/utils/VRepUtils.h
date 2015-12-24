a080a954a259ea9c9f4ccdcd915c4dfefe5437a5
Tiago Rafael Volkmann Coelho <ecomptiago@hotmail.com>
2015-12-24 01:37:58 -0200

    Adicionando metodos utilitariso reutilizaveis

---
+++ trunk/catkin_ws/src/common/include/common/utils/VRepUtils.h
@@ -0,0 +1,61 @@
+/*
+ * VRepUtils.h
+ *
+ *  Created on: Dec 21, 2015
+ *      Author: tiago
+ */
+
+#ifndef INCLUDE_COMMON_UTILS_VREPUTILS_H_
+#define INCLUDE_COMMON_UTILS_VREPUTILS_H_
+
+#include "map"
+#include "iostream"
+#include "ros/ros.h"
+#include "common/simRosGetObjectHandle.h"
+#include "common/simRosGetObjectPose.h"
+
+
+class VRepUtils {
+
+	public:
+		//Constructor
+		VRepUtils() {};
+
+		//Destructor
+		virtual ~VRepUtils() {};
+
+		//Methods
+		static bool getObjectHandle(const char* objectHandleName,
+			ros::NodeHandle &nodeHandle,
+			std::map<std::string,int32_t> &signalObjectMap) {
+				common::simRosGetObjectHandle simRosGetObjectHandle;
+				simRosGetObjectHandle.request.objectName = objectHandleName;
+				ros::ServiceClient client = nodeHandle.serviceClient
+					<common::simRosGetObjectHandle>("/vrep/simRosGetObjectHandle");
+				client.call(simRosGetObjectHandle);
+				if(simRosGetObjectHandle.response.handle != -1) {
+					ROS_DEBUG("Got %d int handle for %s", simRosGetObjectHandle.response.handle,
+						objectHandleName);
+					signalObjectMap[objectHandleName] = simRosGetObjectHandle.response.handle;
+					return true;
+				} else {
+					return false;
+				}
+		}
+
+		static bool getObjectPose(int32_t objectHandle,ros::NodeHandle &nodeHandle,
+			common::simRosGetObjectPose &simRosGetObjectPose) {
+				simRosGetObjectPose.request.handle = objectHandle;
+				simRosGetObjectPose.request.relativeToObjectHandle = -1;
+				ros::ServiceClient client = nodeHandle.serviceClient
+					<common::simRosGetObjectPose>("/vrep/simRosGetObjectPose");
+				client.call(simRosGetObjectPose);
+				if(simRosGetObjectPose.response.result != -1) {
+					return true;
+				} else {
+					return false;
+				}
+		}
+};
+
+#endif /* INCLUDE_COMMON_UTILS_VREPUTILS_H_ */
---
+++ trunk/catkin_ws/src/common/src/utils/VRepUtils.cpp
@@ -0,0 +1,8 @@
+/*
+ * VRep.cpp
+ *
+ *  Created on: Dec 21, 2015
+ *      Author: tiago
+ */
+
+#include "../../include/common/utils/VRepUtils.h"
